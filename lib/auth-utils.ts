b90ebc0e25a6ea6ada239bc9f820c085a1b14f5a
mangochops <133221712+mangochops@users.noreply.github.com>
2025-04-07 10:28:28 +0300

    Delete lib/auth-utils.ts

--- lib/auth-utils.ts
+++
@@ -1,8 +0,0 @@
-import { authOptions } from "@/lib/auth"
-import { getServerSession as getServerSessionNextAuth } from "next-auth/next"
-
-// Helper function to get the session in server components
-export async function getServerSession() {
-  return await getServerSessionNextAuth(authOptions)
-}
-
