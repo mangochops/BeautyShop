dc57b816ed9e4aef0238c70498fd9367efd66d16
mangochops <133221712+mangochops@users.noreply.github.com>
2025-04-07 10:29:37 +0300

    Delete lib/format.ts

--- lib/format.ts
+++
@@ -1,9 +0,0 @@
-export function formatCurrency(amount: number): string {
-  return new Intl.NumberFormat("en-KE", {
-    style: "currency",
-    currency: "KES",
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
-  }).format(amount)
-}
-
